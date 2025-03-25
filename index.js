920f691ae69b198ebdf999968b203c89def78267
ssudaya0 <ssudaya0@gmail.com>
2025-03-26 00:02:41 +0900

    test: diff check

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ import Parser from "rss-parser";
  */
 let text = `# Hi there 👋
 
+## 이런 환경에 익숙해요✍🏼
+
+## 언어
+
+<p>
+  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
+  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
+</p>
+
 ## 📕 Latest Blog Posts
 
 `;
