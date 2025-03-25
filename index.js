72aeddabee350edcdb43ccd0ec001123919b0b3b
ssudaya0 <ssudaya0@gmail.com>
2025-03-25 23:46:47 +0900

    fix: if file count is smaller than 10

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ const parser = new Parser({
   text += `<ul>`;
 
   // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
-  for (let i = 0; i < 10; i++) {
+  const postCount = Math.min(feed.items.length, 10);
+  for (let i = 0; i < postCount; i++) {
     const { title, link } = feed.items[i];
     console.log(`${i + 1}번째 게시물`);
     console.log(`추가될 제목: ${title}`);
